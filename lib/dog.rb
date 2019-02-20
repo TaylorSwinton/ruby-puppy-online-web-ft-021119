b16021a1a63ac5dce6f2f2da8c45517d65444ab5
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-20 01:45:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,8 @@
 class Dog
+
+    attr_reader :name
     @@all=[]
+    
   def initialize(name)
     @name = name
     @@all << self
@@ -12,7 +15,7 @@ class Dog
   def self.all
 
     @@all.each do |dog|
-      puts dog
+      puts dog.name
     end
 
   end
